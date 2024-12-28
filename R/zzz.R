4ba50d94693f1d9efe04002e0cd78e3abebb4f4c
Jason Doll <jason.doll@fmarion.edu>
2024-12-28 17:41:07 -0500

    added catch isopleth and fixed build warnings.

--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,3 @@
 .onAttach <- function(libname, pkgname) {
-  packageStartupMessage("Welcome to FAMSr, the R version of FAMS! We are currently in development.")
+  packageStartupMessage("Welcome to rFAMS, the R version of FAMS! We are currently in development.")
 }
--- tests/Test_Code.R
+++ tests/Test_Code.R
@@ -24,9 +24,9 @@
 #            cmmin = 0.05,
 #            cmmax = 0.95,
 #            cminc = 0.05,
-#            lengthmin = 200,
+#            lengthmin = 100,
 #            lengthmax = 600,
-#            lengthinc= 25,
+#            lengthinc= 50,
 #            initialN=100,
 #            linf=2000,
 #            K=0.50,
@@ -95,54 +95,43 @@
 #   )
 #
 #
-# # #Simulate yield based on a range minimum length and range of exploitation
-# # #minimum length range
-# # minlength <- seq(from=200, to=700, by=10)
-# #
-# # #initialize list
-# # Res_2 <- list()
-# #
-# # #loop through minimum lengths
-# # for(x in 1:length(minlength)){
-# #   Res_2[[x]]<-ypr(
-# #     cfmin=0.05,cfmax=0.95,cfinc=0.05,
-# #     cm=0.40,
-# #     minlength=minlength[x],                  #Minimum length limit
-# #     initialN=100,                   #Number of new recruits entering the fishery
-# #     linf=2000,                      #Point estimate of Linf from LVB
-# #     K=0.250,                         #Point estimate of K from LVB
-# #     t0=-0.616,                      #Point estimate of t0 from LVB
-# #     LWalpha=-5.453,              #Point estimate of alpha from LW regression
-# #     LWbeta=3.10,                    #Point estimate of beta from LW regression
-# #     Mage=15                        #Maximum age of fish in population
-# #   )
-# # }
-# #
-# # #Pull out yield from each list item
-# # yield_df <- matrix(nrow=length(minlength),ncol=length(Res_2[[1]]$uvect))
-# # for(x in 1:length(minlength)){
-# #   yield_df[x,] <- Res_2[[x]]$yieldall
-# # }
-# #
-# # #Convert to long format
-# # yield_df <- data.frame(yield_df)
-# # names(yield_df) <- Res_2[[1]]$uvect
-# #
-# # yield_df <- yield_df %>% pivot_longer(cols = names(yield_df)[1]:names(yield_df)[ncol(yield_df)]) %>%
-# #   mutate(MLL = sort(rep(minlength,ncol(yield_df)))) %>%
-# #   mutate_at(c('name'), as.numeric)
-# #
-# # #Yield isopleth
-# # ggplot(data = yield_df) +
-# #   theme_bw()+theme(panel.grid.major=element_blank(),panel.grid.minor=element_blank())+
-# #   geom_contour(aes(x=as.numeric(name),y=MLL,z=value))+
-# #   metR::geom_text_contour(aes(x=as.numeric(name),y=MLL,z = value),stroke = 0.15)+
-# #   xlab("Exploitation")+
-# #   ylab("Minimum length limit (mm)")+
-# #   theme(axis.text.x=element_text(size=20),
-# #         axis.text.y=element_text(size=20),
-# #         axis.title.x=element_text(size=22),
-# #         axis.title.y=element_text(size=22,angle=90),
-# #         panel.border = element_blank(),
-# #         axis.line = element_line(colour = "black")
-# #   )
+# #Number Harvested Isopleth
+# #Extract exploitation and yield from each MLL and CF with cm = 0.40
+# #into a dataframe with one column of yield for each row is length limit and each column is exploitation
+#
+# #Does not match FAMS number harvest isopleth.
+# #FAMS has number harvested/caught > initial number(N0). This can't be possible...
+#
+# targetcm = 0.8
+#
+# minlength <- Res_1[[1]]$MLvect
+#
+# exploitation <- Res_1[[3]]$exploitation[,which(Res_1[[3]]$cmvect==targetcm)]
+#
+# Catch_df <- matrix(nrow=length(Res_1),ncol=length(exploitation))
+# for(x in 1:length(Res_1)){
+#   Catch_df[x,] <- Res_1[[x]]$Nharvest[,which(Res_1[[3]]$cmvect==targetcm)]
+# }
+#
+# #Convert to long format
+# Catch_df <- data.frame(Catch_df)
+# names(Catch_df) <- exploitation
+#
+# Catch_df <- Catch_df %>% pivot_longer(cols = names(Catch_df)[1]:names(Catch_df)[ncol(Catch_df)]) %>%
+#   mutate(MLL = sort(rep(minlength,ncol(Catch_df)))) %>%
+#   mutate_at(c('name'), as.numeric)
+#
+# #Yield isopleth
+# ggplot(data = Catch_df) +
+#   theme_bw()+theme(panel.grid.major=element_blank(),panel.grid.minor=element_blank())+
+#   geom_contour(aes(x=as.numeric(name),y=MLL,z=value))+
+#   metR::geom_text_contour(aes(x=as.numeric(name),y=MLL,z = value),stroke = 0.15)+
+#   xlab("Exploitation")+
+#   ylab("Minimum length limit (mm)")+
+#   theme(axis.text.x=element_text(size=20),
+#         axis.text.y=element_text(size=20),
+#         axis.title.x=element_text(size=22),
+#         axis.title.y=element_text(size=22,angle=90),
+#         panel.border = element_blank(),
+#         axis.line = element_line(colour = "black")
+#   )
